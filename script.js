bacd95e72566f74ed3127035a9b0c61b744d261d
Joshua-1000 <68680246+Joshua-1000@users.noreply.github.com>
2022-06-13 19:57:34 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+function changingText() {
+    w = document.getElementById("changeText")
+    if (w.innerHTML === 'JavaScript'){
+        w.innerHTML = "JavaScript is a Programming Language";
+        
+    }
+    else {
+        w.innerHTML = "JavaScript";
+    }
+}
